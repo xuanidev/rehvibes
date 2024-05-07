93ce31b3b06a845d27b3bb87672f889563693938
Juan Ignacio <juasosua@gmail.com>
2024-05-08 01:15:26 +0200

    feat: design system, typography colors

--- src/layouts/DesignSystem.tsx
+++ src/layouts/DesignSystem.tsx
@@ -5,24 +5,88 @@ import { toastError } from "../constants";
 export const DesignSystem = () => {
   return (
     <div>
-      <section>
+      <section className="ds_section">
         <h2>1. Colores</h2>
-        <div style={{ backgroundColor: "#007bff", color: "#fff" }}>
-          Contenido con color de fondo primario
+        <div className="colors_section__content">
+          <div className="content__color bg_primary">
+            <h3>Primary</h3>
+            <p>hsl(32, 100%, 61%)</p>
+            <p>Text: black</p>
+          </div>
+          <div className="content__color bg_primary_light">
+            <h3>Primary Light</h3>
+            <p>hsl(32, 94%, 88%)</p>
+            <p>Text: black</p>
+          </div>
+          <div className="content__color bg_fluorescent">
+            <h3>Fluorescent</h3>
+            <p>hsl(16, 100%, 45%)</p>
+            <p>Text: white</p>
+          </div>
+          <div className="content__color bg_secondary">
+            <h3>Secondary</h3>
+            <p>hsl(16, 100%, 59%)</p>
+            <p>Text: white</p>
+          </div>
+          <div className="content__color bg_secondary_dark">
+            <h3>Secondary Dark</h3>
+            <p>hsl(16, 70%, 49%)</p>
+            <p>Text: white</p>
+          </div>
         </div>
       </section>
 
-      <section>
+      <section className="ds_section">
         <h2>2. Tipografía</h2>
-        <p
-          style={{
-            fontFamily: "Arial",
-            fontSize: "16px",
-            fontWeight: "normal",
-          }}
-        >
-          Texto de ejemplo
-        </p>
+        <ul className="typography_section">
+          <li className="header_one">
+            <p>H1: Header One</p>
+            <ul className="sizes">
+              <li>Size 'sm': 26px</li>
+              <li>Size 'md': 26px</li>
+              <li>Size 'lg': 26px</li>
+              <li>Size 'xl': 40px</li>
+            </ul>
+          </li>
+          <li className="header_two">
+            <p>H2: Header Two</p>
+            <ul className="sizes">
+              <li>Size 'sm': 20px</li>
+              <li>Size 'md': 26px</li>
+              <li>Size 'lg': 26px</li>
+              <li>Size 'xl': 30px</li>
+            </ul>
+          </li>
+          <li className="header_three">
+            <p>H3: Header Three</p>
+            <ul className="sizes">
+              <li>Size 'sm': 20px</li>
+              <li>Size 'md': 20px</li>
+              <li>Size 'lg': 20px</li>
+              <li>Size 'xl': 26px</li>
+            </ul>
+          </li>
+          <li className="bodies">
+            <p className="bodies__bold">Body Bold</p>
+            <p className="bodies__body">Body</p>
+            <p className="bodies__light">Body Light</p>
+            <ul className="sizes">
+              <li>Size 'sm': 14px</li>
+              <li>Size 'md': 16x</li>
+              <li>Size 'lg': 16x</li>
+              <li>Size 'xl': 16x</li>
+            </ul>
+          </li>
+          <li className="button_meta">
+            <p>Button & Meta</p>
+            <ul className="sizes">
+              <li>Size 'sm': 15px</li>
+              <li>Size 'md': 16px</li>
+              <li>Size 'lg': 18px</li>
+              <li>Size 'xl': 20px</li>
+            </ul>
+          </li>
+        </ul>
       </section>
 
       <section>
