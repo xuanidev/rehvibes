595dc3bda9303e47c6927f2702606283c237ece4
Juan Ignacio <juasosua@gmail.com>
2024-05-22 21:58:30 +0200

    feat: body parts map component

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,5 @@
 import { ToastOptions, ToastPosition } from "react-toastify";
 
-const days = ["Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"];
-
-const months = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-];
-
-
 const toastError: ToastOptions<ToastPosition> = {
     position: "top-center",
     hideProgressBar: true,
@@ -33,4 +15,24 @@ const toastError: ToastOptions<ToastPosition> = {
     },
   };
 
-export {days, months, toastError} 
+const bodyPartsMap = {
+  'Cuello': ['neck'],
+  'Muñeca': ['forearm'],
+  'Hombro': ['back-deltoids', 'front-deltoids'],
+  'Rodilla': [''],
+  'Espalda': ['trapezius', 'upper-back', 'lower-back'],
+  'Pie': [''],
+  'Cadera': ['abductors', 'adductor'],
+  'Tobillo': ['calves'],
+  'Codo': ['biceps', 'triceps'],
+  'Columna vertebral': ['upper-back', 'lower-back'],
+  'Lumbar': ['lower-back'],
+  'Isquios': ['hamstring'],
+  'Pelvis': ['adductor', 'abductors'],
+  'Pectoral': ['chest'],
+  'Tobillos': ['calves'],
+  'Pies': [''],
+  'Gluteos': ['gluteal']
+};
+
+export {toastError, bodyPartsMap} 
