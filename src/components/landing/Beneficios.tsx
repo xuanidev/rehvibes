9392b1f25684f147cbe82e1b966eae2dc7318dc7
José <148952612+jocofe@users.noreply.github.com>
2024-06-11 20:33:03 +0200

    feat: slides landing

--- src/components/landing/Beneficios.tsx
+++ src/components/landing/Beneficios.tsx
@@ -4,7 +4,14 @@ import './beneficios.scss';
 
 export const Beneficios = () => {
     return (
-        <CardBeneficios tittle='tiulo' text='hola' />
+        <div className="section_Beneficios">
+            <h2 className='tittle_H2'>Beneficios</h2>
+            <p className='text_landing'>Descubre todos los beneficios de la telerrehabilitación con Revibes.  A tu ritmo, con tus horarios y con ejercicios y rutinas diseñados por profesionales de la salud y el deporte.</p>
+            <div className='grid_items'>
+               
+            </div>
+        </div>
+
     );
 };
 export default Beneficios;
