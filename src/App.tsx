5c324d72e59a3aa2c49bad4c44d0bc282d72f07b
Juan Ignacio <juasosua@gmail.com>
2024-06-20 02:04:12 +0200

    fix: remove unused line

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './styles/style.scss';
 import SideBar from './components/Sidebar';
-import { getFromCookies } from './utils/helpers';
 import { ModalProvider } from './contexts/ModalContext';
 
 export const App = () => {
